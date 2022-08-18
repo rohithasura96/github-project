a692003abfd394c1babad8a34a713ab16ee39da2
Rohit <rohit@hasura.io>
2022-08-18 15:31:32 +0530

    fixes decreament

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 
 function App() {
   const [count,  effat, ] = useState(0);
- 
- 
 
-  
+  const incNum = () => {
+    if (count< 1000) {
+      effat(count +1)
+
+    }
+  }
+
+  const decNum = () => {
+      if (count > 0){
+        effat(count -1)
+      }
+  }
+ 
   return (
     <div className="App">
       <div className="App-co">
        <p>{count}</p>
-       <button className='btn' onClick={
-        () => effat (count +1)
-       }>Click +</button>
-       <button className='btn' onClick={
-        () => effat (count -1)
-       }>Click -</button>
+       <button className='btn' onClick={incNum}>Click +</button>
+       <button className='btn' onClick={ decNum}>Click -</button>
 
        <button className='btn'type='reset' onClick={
         () => effat(0)
